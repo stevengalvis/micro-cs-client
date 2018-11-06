ce48bdb1839c58866d5c9b0ba45eb7d0c68745db
stevengalvis <steven.galvis@gmail.com>
2018-11-05 21:54:58 -0500

    initialized nav component and added lorem ipsum to about component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,53 @@
 import React from "react";
 
 export const About = props => {
-  return <div className="about" />;
+  return (
+    <div className="about">
+      <div className="about-content">
+        <h2>Why learn Data Structures and Algorithms?</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.{" "}
+        </p>
+      </div>
+      <div className="about-content">
+        <h2>Why Micro CS?</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.{" "}
+        </p>
+      </div>
+      <div className="about-content">
+        <h2>Micro CS Example</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.{" "}
+        </p>
+      </div>
+      <div className="about-content">
+        <h2>Overview of topics</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.{" "}
+        </p>
+      </div>
+      <div className="about-content">
+        <h2>Roadmap Path</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.{" "}
+        </p>
+      </div>
+    </div>
+  );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import About from "../About/About";
+import Nav from "../Nav/Nav";
 
 export const Home = props => {
   return (
     <div className="home">
+      <Nav />
       <About />
     </div>
   );
